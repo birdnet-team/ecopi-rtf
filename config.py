04a14edfd978dfdeeffaa2857312e0740eaf95c9
Josef Haupt <josef.haupt@phil.tu-chemnitz.de>
2024-10-14 15:08:50 +0200

    use dotenv for secrets

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import json
+import os
 
-def load_api_token():
-    with open('auth.json') as f:
-        data = json.load(f)
-        return data['api-token'] 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def load_species_data():
     with open('assets/amic_species_info.json') as f:
         data = json.load(f)
         return data
 
-API_TOKEN = load_api_token()
+API_TOKEN = os.getenv('API_TOKEN')
 API_BASE_URL = 'https://api.ecopi.de/api/v0.1/' #https://api.ecopi.de/api/v0.1/meta/project/{project_name}/detections/recorderspeciescounts/
 PROJECT_NAME = 'pam_in_chemnitz'
 SPECIES_DATA = load_species_data()
