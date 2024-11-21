d424e93dbb4ecc2f84d82b625bce87a664577cc7
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2024-11-21 18:20:22 +0100

    refactor links

--- widgets/footer.py
+++ widgets/footer.py
@@ -14,17 +14,14 @@ def footer_content():
                         dbc.Col(
                             [
                                 html.H6("Site Links", className="small-text bold-text"),
-                                html.Div(
+                                html.Ul(
                                     [
-                                        html.A("Home", href=f"{cfg.SITE_ROOT}/", className="footer-link small-text"),
-                                        html.Br(),
-                                        html.A("Detections", href=f"{cfg.SITE_ROOT}/detections", className="footer-link small-text"),
-                                        html.Br(),
-                                        html.A("Recording units", href=f"{cfg.SITE_ROOT}/recorder/1", className="footer-link small-text"),
-                                        html.Br(),
-                                        html.A("About the project", href=f"{cfg.SITE_ROOT}/about", className="footer-link small-text"),
+                                        html.Li(html.A("Home", href=f"{cfg.SITE_ROOT}/", className="footer-link small-text")),
+                                        html.Li(html.A("Detections", href=f"{cfg.SITE_ROOT}/detections", className="footer-link small-text")),
+                                        html.Li(html.A("Recording units", href=f"{cfg.SITE_ROOT}/recorder/1", className="footer-link small-text")),
+                                        html.Li(html.A("About the project", href=f"{cfg.SITE_ROOT}/about", className="footer-link small-text")),
                                     ],
-                                    className=""
+                                    className="list-unstyled"
                                 )
                             ],
                             width=6,
@@ -35,21 +32,16 @@ def footer_content():
                         dbc.Col(
                             [
                                 html.H5("K. Lisa Yang Center for Conservation Bioacoustics", className="small-text bold-text"),
-                                html.Div(
+                                html.Ul(
                                     [
-                                        html.A("Center", href="https://www.birds.cornell.edu/ccb/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Our values", href="https://www.birds.cornell.edu/ccb/our-values/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Research", href="https://www.birds.cornell.edu/ccb/research/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Technology", href="https://www.birds.cornell.edu/ccb/technology/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Education", href="https://www.birds.cornell.edu/ccb/education/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Publications", href="https://www.birds.cornell.edu/ccb/publications/", className="footer-link small-text", target="_blank"),
+                                        html.Li(html.A("Center", href="https://www.birds.cornell.edu/ccb/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Our values", href="https://www.birds.cornell.edu/ccb/our-values/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Research", href="https://www.birds.cornell.edu/ccb/research/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Technology", href="https://www.birds.cornell.edu/ccb/technology/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Education", href="https://www.birds.cornell.edu/ccb/education/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Publications", href="https://www.birds.cornell.edu/ccb/publications/", className="footer-link small-text", target="_blank")),
                                     ],
-                                    className=""
+                                    className="list-unstyled"
                                 )
                             ],
                             width=6,
@@ -60,17 +52,14 @@ def footer_content():
                         dbc.Col(
                             [
                                 html.H5("Explore More", className="small-text bold-text"),
-                                html.Div(
+                                html.Ul(
                                     [
-                                        html.A("Bird Cams", href="https://www.allaboutbirds.org/cams/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Birds of the World", href="https://birdsoftheworld.org/bow/home", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("eBird Status and Trends", href="https://science.ebird.org/status-and-trends", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Our Youtube Videos", href="https://www.youtube.com/labofornithology", className="footer-link small-text", target="_blank"),
+                                        html.Li(html.A("Bird Cams", href="https://www.allaboutbirds.org/cams/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Birds of the World", href="https://birdsoftheworld.org/bow/home", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("eBird Status and Trends", href="https://science.ebird.org/status-and-trends", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Our Youtube Videos", href="https://www.youtube.com/labofornithology", className="footer-link small-text", target="_blank")),
                                     ],
-                                    className=""
+                                    className="list-unstyled"
                                 )
                             ],
                             width=6,
@@ -81,17 +70,14 @@ def footer_content():
                         dbc.Col(
                             [
                                 html.H5("Lifelong Learning", className="small-text bold-text"),
-                                html.Div(
+                                html.Ul(
                                     [
-                                        html.A("Online Courses", href="https://academy.allaboutbirds.org/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Bird Walks & Events", href="https://www.birds.cornell.edu/home/visit/events/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Spring Field Ornithology", href="https://academy.allaboutbirds.org/product/spring-field-ornithology-northeast/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("K–12 Education", href="https://www.birds.cornell.edu/k12", className="footer-link small-text", target="_blank"),
+                                        html.Li(html.A("Online Courses", href="https://academy.allaboutbirds.org/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Bird Walks & Events", href="https://www.birds.cornell.edu/home/visit/events/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Spring Field Ornithology", href="https://academy.allaboutbirds.org/product/spring-field-ornithology-northeast/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("K–12 Education", href="https://www.birds.cornell.edu/k12", className="footer-link small-text", target="_blank")),
                                     ],
-                                    className=""
+                                    className="list-unstyled"
                                 )
                             ],
                             width=6,
@@ -102,19 +88,15 @@ def footer_content():
                         dbc.Col(
                             [
                                 html.H5("Support Our Cause", className="small-text bold-text"),
-                                html.Div(
+                                html.Ul(
                                     [
-                                        html.A("Join the Lab", href="https://join.birds.cornell.edu/page/150611/donate/1?ea.tracking.id=WXXXXX14C", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Donate", href="https://give.birds.cornell.edu/page/132162/donate/1?ea.tracking.id=ENR", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Monthly Giving", href="https://give.birds.cornell.edu/page/99134/donate/1?ea.tracking.id=BCF", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Membership Services", href="https://www.birds.cornell.edu/home/members/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Shop for Our Cause", href="https://www.birds.cornell.edu/home/shop-for-our-cause/", className="footer-link small-text", target="_blank"),
+                                        html.Li(html.A("Join the Lab", href="https://join.birds.cornell.edu/page/150611/donate/1?ea.tracking.id=WXXXXX14C", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Donate", href="https://give.birds.cornell.edu/page/132162/donate/1?ea.tracking.id=ENR", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Monthly Giving", href="https://give.birds.cornell.edu/page/99134/donate/1?ea.tracking.id=BCF", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Membership Services", href="https://www.birds.cornell.edu/home/members/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Shop for Our Cause", href="https://www.birds.cornell.edu/home/shop-for-our-cause/", className="footer-link small-text", target="_blank")),
                                     ],
-                                    className=""
+                                    className="list-unstyled"
                                 )
                             ],
                             width=6,
@@ -125,17 +107,14 @@ def footer_content():
                         dbc.Col(
                             [
                                 html.H5("About", className="small-text bold-text"),
-                                html.Div(
+                                html.Ul(
                                     [
-                                        html.A("Cornell Lab of Ornithology", href="http://birds.cornell.edu/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Web Accessibility Assistance", href="https://www.birds.cornell.edu/home/web-accessibility-assistance/", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Privacy Policy", href="https://privacy.cornell.edu/information-use-cornell", className="footer-link small-text", target="_blank"),
-                                        html.Br(),
-                                        html.A("Terms of Use", href="https://www.birds.cornell.edu/home/terms-of-use/", className="footer-link small-text", target="_blank"),
+                                        html.Li(html.A("Cornell Lab of Ornithology", href="http://birds.cornell.edu/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Web Accessibility Assistance", href="https://www.birds.cornell.edu/home/web-accessibility-assistance/", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Privacy Policy", href="https://privacy.cornell.edu/information-use-cornell", className="footer-link small-text", target="_blank")),
+                                        html.Li(html.A("Terms of Use", href="https://www.birds.cornell.edu/home/terms-of-use/", className="footer-link small-text", target="_blank")),
                                     ],
-                                    className=""
+                                    className="list-unstyled"
                                 )
                             ],
                             width=6,
