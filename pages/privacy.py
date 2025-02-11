b0fa44043acf348a004e3534351b6f0a9e54dd93
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2025-02-11 14:56:13 +0100

    remove site root

--- pages/privacy.py
+++ pages/privacy.py
@@ -8,11 +8,11 @@ import config as cfg
 def privacy_page_content(locale):
     
     # Construct the file path for the privacy policy markdown file
-    file_path = f"{cfg.SITE_ROOT}assets/text/privacy_policy_{locale}.md"
+    file_path = f"assets/text/privacy_policy_{locale}.md"
     
     # Check if the file exists, if not, fall back to the English version
     if not os.path.exists(file_path):
-        file_path = f"{cfg.SITE_ROOT}assets/text/privacy_policy_en.md"
+        file_path = f"assets/text/privacy_policy_en.md"
     
     # Read the privacy policy markdown file
     with open(file_path, 'r') as file:
