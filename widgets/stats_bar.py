b308ef67d213709054364042b8feab089c757423
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2025-04-24 14:21:23 +0200

    loading spinner

--- widgets/stats_bar.py
+++ widgets/stats_bar.py
@@ -7,29 +7,32 @@ def stats_bar(locale):
     
     strings = Strings(locale)
     
+    # Create a loading spinner element
+    loading_spinner = html.I(className="bi bi-arrow-repeat spinner-icon")
+    
     return html.Div(
         dbc.Row(
             [
                 dbc.Col(
-                    html.Div([html.Div(f"{strings.get('stats_bar_detections_24h')}:", className='stats-bar-headline'), html.H4(id="detections-24h", children="-")]),
+                    html.Div([html.Div(f"{strings.get('stats_bar_detections_24h')}:", className='stats-bar-headline'), html.H4(id="detections-24h", children=loading_spinner)]),
                     className="stat-column",
                     width=6,
                     md=3,
                 ),
                 dbc.Col(
-                    html.Div([html.Div(f"{strings.get('stats_bar_species_24h')}:", className='stats-bar-headline'), html.H4(id="species-24h", children="-")]),
+                    html.Div([html.Div(f"{strings.get('stats_bar_species_24h')}:", className='stats-bar-headline'), html.H4(id="species-24h", children=loading_spinner)]),
                     className="stat-column",
                     width=6,
                     md=3,
                 ),
                 dbc.Col(
-                    html.Div([html.Div(f"{strings.get('stats_bar_detections_total')}:", className='stats-bar-headline'), html.H4(id="total-detections", children="-")]),
+                    html.Div([html.Div(f"{strings.get('stats_bar_detections_total')}:", className='stats-bar-headline'), html.H4(id="total-detections", children=loading_spinner)]),
                     className="stat-column",
                     width=6,
                     md=3,
                 ),
                 dbc.Col(
-                    html.Div([html.Div(f"{strings.get('stats_bar_audio_total')}:", className='stats-bar-headline'), html.H4(id="total-audio", children="-")]),
+                    html.Div([html.Div(f"{strings.get('stats_bar_audio_total')}:", className='stats-bar-headline'), html.H4(id="total-audio", children=loading_spinner)]),
                     className="stat-column",
                     width=6,
                     md=3,
